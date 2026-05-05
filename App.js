a5b553edfadd56f2f59fc292d6ab33bc55a46e05
eduardocolella <eduardo310301@edu.unipar.br>
2026-05-05 19:35:01 -0300

    Ajsuts

--- App.js
+++ App.js
@@ -1,44 +1,40 @@
-import { StyleSheet, Text, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image, ImageBackground, SectionList } from 'react-native';
 import GameCard from './components/GameCard';
+import dados from './assets/dados.json'
 
 export default function App() {
 
-  const jogos = [
-    {
-      "id": 1,
-      "fase": "Fase de grupos",
-      "grupo": "A",
-      "data_et": "2026-06-11",
-      "hora_et": "15:00",
-      "data_brasilia": "2026-06-11",
-      "hora_brasilia": "16:00",
-      "time_casa": "México",
-      "sigla_casa": "MEX",
-      "time_fora": "África do Sul",
-      "sigla_fora": "RSA",
-      "confronto": "México x África do Sul",
-      "estadio": "Estádio Azteca",
-      "cidade": "Cidade do México",
-      "pais": "México"
-    },
-    {
-      "id": 2,
-      "fase": "Fase de grupos",
-      "grupo": "A",
-      "data_et": "2026-06-11",
-      "hora_et": "22:00",
-      "data_brasilia": "2026-06-11",
-      "hora_brasilia": "23:00",
-      "time_casa": "Coreia do Sul",
-      "sigla_casa": "KOR",
-      "time_fora": "Tchéquia",
-      "sigla_fora": "CZE",
-      "confronto": "Coreia do Sul x Tchéquia",
-      "estadio": "Estádio Akron",
-      "cidade": "Guadalajara",
-      "pais": "México"
+  const jogos = dados.jogos;
+
+  const agruparPorData =(jogos) => {
+    
+    return jogos.reduce((acc, jogo) => {
+
+      const data = jogo.data_brasilia;
+
+      if (!acc[data]) {
+        acc[data] = [];
+      }
+
+      acc[data].push(jogo);
+
+      return acc;
+
+    }, {});
+
+  }
+
+  const jogosAgrupados = agruparPorData(jogos);
+  
+  const jogosTratados = Objectkeys(jogosAgrupados).map(data => {
+    return {
+      title: data,
+      jogos: jogosAgrupados[data]
     }
-  ]
+  });
+
+  console.log(jogosTratados)
+  
 
   return (
     <ImageBackground style={styles.container}
@@ -49,17 +45,29 @@ export default function App() {
 
       <Text style={styles.title}>CALENDÁRIO</Text>
 
-      <View style={styles.card}>
+      <SectionList
+      sections={jogosTratados}
+      keyExtractor={(item, index) => item + index}
+      renderItem={() => null}
+      renderSectionHeader={ ({section}) => (
+        <View style = {styles.card}>
+        <Text style={styles.data}>{section.title}</Text>
 
-        <Text style={styles.data}>
-          {jogos[0].data_brasilia.split('-').slice(1).reverse().join('/')}
-        </Text>
-        
-        <GameCard game={jogos[0]}/>
-        <GameCard game={jogos[1]}/>
+          
+          {
+            section.data.map[jogo => (
+              <GameCard key={jogo.id} game= {jogo} />
+            )]
+          }
 
+        </View>
+      
 
-      </View>
+        )
+    }
+
+      />
+
     </ImageBackground>
   );
 }
